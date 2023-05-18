96bdadf49cf07d124ad1474a8bc730f48ddc92cb
Tushar Verma <tuhsar2022@gmail.com>
2023-05-18 18:12:31 +0530

    submitting code-Thu May 18 2023 18:12:31 GMT+0530 (India Standard Time)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p>I am learning React. My life is getting better.</p>
+      <h1>hi </h1>
+    </div>
   )
 }
 
